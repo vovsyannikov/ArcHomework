20337f33ec8cf20d3666a4b452313ee20a68b30c
Виталий <its.vint@ya.ru>
2020-10-10 13:19:28 +0300

    Подготовка

---
+++ ArcHomework/Models/BSWModel.swift
@@ -0,0 +1,12 @@
+//
+//  BSWModel.swift
+//  ArcHomework
+//
+//  Created by Виталий Овсянников on 10.10.2020.
+//
+
+import Foundation
+
+class BSWModel{
+    
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'ArcHomework' do
+  use_frameworks!
+
+pod 'Alamofire'
+
+end
